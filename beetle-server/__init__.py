ead4ee0033baa7b39dca5f9a4cfe42874f3e852b
Esben Sonne <esbensonne@gmail.com>
2014-08-14 21:55:36 +0200

    Initial prototype.

---
+++ beetle-server/__init__.py
@@ -0,0 +1,24 @@
+import SimpleHTTPServer
+import SocketServer
+import os
+
+
+class Server:
+    def __init__(self, own_config, config, builder):
+        self.directory = config.folders['output']
+        self.port = own_config['port']
+        self.builder = builder
+
+    def serve(self):
+        os.chdir(self.directory)
+
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+        httpd = SocketServer.TCPServer(('', self.port), Handler)
+
+        httpd.serve_forever()
+
+
+def register(plugin_config, config, commander, builder, content_renderer):
+    server = Server(plugin_config, config, builder)
+    commander.add('serve', server.serve)
