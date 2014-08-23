d982d612ac645c2f7c1a99daa95a57428445b6e6
Jeppe Toustrup <jeppe@tenzer.dk>
2014-08-23 10:47:59 +0200

    Added basic setup.py to allow easier package installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='beetle_preview',
+    author='Esben Sonne',
+    author_email='esbensonne@gmail.com',
+    packages=[
+        'beetle_preview'
+    ],
+)
