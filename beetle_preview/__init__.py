8c4d4439971516f5adedd816fe264a700509ad8e
Esben Sonne <esbensonne@gmail.com>
2014-08-31 19:02:54 +0200

    Updated to test the new plugin method.

--- beetle_preview/__init__.py
+++ beetle_preview/__init__.py
@@ -1,14 +1,55 @@
+from beetle import Commander, Writer
+
 from http import server
 from socketserver import TCPServer
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+from hashlib import md5
 import os
 
+class Updater(FileSystemEventHandler):
+    cache = {}
+    writer = Writer()
+
+    def __init__(self, serve_directory):
+        self.directory = serve_directory
+
+    def on_any_event(self, event):
+        # Urgh, ugly directory hack.
+        # Could not find an easy way to serve files from a subfolder.
+        os.chdir('..')
+
+        for destination, content in self.writer.files():
+            digest = md5(content).hexdigest()
+            if destination not in self.cache:
+                self.writer.write_file(destination, content)
+                self.cache[destination] = digest
+                print('written')
+            elif self.cache[destination] != digest:
+                    self.writer.write_file(destination, content)
+                    self.cache[destination] = digest
+                    print('updated')
+            else:
+                print('cached')
+
+        os.chdir(self.directory)
 
 class Server:
     def __init__(self, own_config, config):
         self.directory = config.folders['output']
+        self.content = config.folders['content']
         self.port = own_config.get('port', 5000)
 
+    def monitor(self):
+        updater = Updater(self.directory)
+
+        observer = Observer()
+        observer.schedule(updater, self.content, recursive=True)
+        observer.start()
+
     def serve(self):
+        self.monitor()
         os.chdir(self.directory)
 
         request_handler = server.SimpleHTTPRequestHandler
@@ -21,6 +62,6 @@ class Server:
             httpd.shutdown()
 
 
-def register(plugin_config, config, commander, builder, content_renderer):
-    server = Server(plugin_config, config)
-    commander.add('preview', server.serve, 'Serve the rendered site')
+def register(plugin_config, beetle_config):
+    server = Server(plugin_config, beetle_config)
+    Commander.add('preview', server.serve, 'Serve the rendered site')
