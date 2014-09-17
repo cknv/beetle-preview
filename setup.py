ea820f61561da0bcf4217c79d570c7381c3576e1
Esben Sonne <esbensonne@gmail.com>
2014-09-17 16:02:05 +0200

    Changed email.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='beetle_preview',
     author='Esben Sonne',
-    author_email='esbensonne@gmail.com',
+    author_email='esbensonne+code@gmail.com',
     url='https://github.com/cknv/beetle-preview',
     license='MIT',
     packages=[
