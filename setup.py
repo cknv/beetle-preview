ac6fe039a7dd737c6bbb088112f21287508cda68
Esben Sonne <esbensonne@gmail.com>
2014-09-28 15:56:26 +0200

    Added verison.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='beetle_preview',
+    version='0.1.0'
     author='Esben Sonne',
     author_email='esbensonne+code@gmail.com',
     description='Local test server plugin for Beetle',
