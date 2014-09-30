745135b21893f3043c0a5c0456be1c7b15a75f08
Esben Sonne <esbensonne@gmail.com>
2014-09-30 21:14:21 +0200

    Renamed and added classifiers.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='beetle_preview',
+    name='Beetle-Preview',
     version='0.1.0'
     author='Esben Sonne',
     author_email='esbensonne+code@gmail.com',
@@ -10,6 +10,15 @@ setup(
     license='MIT',
     packages=[
         'beetle_preview'
+    ],
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
     ],
     install_requires=[
         'watchdog'
